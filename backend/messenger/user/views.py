3ceb70784534b32199ac9628e895cd846ebeeca5
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2024-07-09 03:45:24 +0300

    add form for reset password

--- backend/messenger/user/views.py
+++ backend/messenger/user/views.py
@@ -12,8 +12,13 @@ from .serializers import CustomPasswordResetConfirmSerializer
 class PasswordResetConfirmView(APIView):
     permission_classes = [AllowAny]
 
+    def get(self, request, uid, token, *args, **kwargs):
+        # Render the password reset form
+        return render(request, template_name='reset_password_form.html')
+
     def post(self, request, uid, token, *args, **kwargs):
         try:
+            # Decode the UID and get the user
             uid = force_str(urlsafe_base64_decode(uid))
             user = CustomUser.objects.get(pk=uid)
         except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
@@ -26,13 +31,15 @@ class PasswordResetConfirmView(APIView):
                 user.save()
                 return Response({"detail": "Password reset successful."}, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
         return Response({"detail": "Invalid token or user ID."}, status=status.HTTP_400_BAD_REQUEST)
-    
+
 class ActivationView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, uid, token, *args, **kwargs):
         try:
+            # Decode the UID and get the user
             uid = force_str(urlsafe_base64_decode(uid))
             user = CustomUser.objects.get(pk=uid)
         except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
@@ -41,6 +48,15 @@ class ActivationView(APIView):
         if user is not None and default_token_generator.check_token(user, token):
             user.is_active = True
             user.save()
-            return render(template_name='account_activate_result.html', request=request, context={"title": "Ваш аккаунт успешно активирован!", "desc":"Теперь вы можете войти и начать пользоваться нашим сервисом."}, status=status.HTTP_200_OK)
-        return render(template_name='account_activate_result.html', request=request, context={"title": "Ошибка 400", "desc":"Попробуйте зарегистрироваться еще раз или обратитесь в техническую поддержку!"}, status=status.HTTP_400_BAD_REQUEST)
-    
+            return render(
+                request,
+                template_name='account_activate_result.html',
+                context={"title": "Ваш аккаунт успешно активирован!", "desc": "Теперь вы можете войти и начать пользоваться нашим сервисом."},
+                status=status.HTTP_200_OK
+            )
+        return render(
+            request,
+            template_name='account_activate_result.html',
+            context={"title": "Ошибка 400", "desc": "Попробуйте зарегистрироваться еще раз или обратитесь в техническую поддержку!"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
